3c1ff3e15356b57a7c8197ffb53345b626431cb7
Seyit Kaan Karaalioglu <kaankaraalioglu@yahoo.com>
2024-04-22 13:02:54 +0200

    initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
+
+urlpatterns = i18n_patterns(
+    path("", include("about_page.urls")),
+    path("polls/", include("polls.urls")), 
+    path("contact/", include("contact_page.urls")),
+    path("about/products", include("products_page.urls")),
+    path("admin/", admin.site.urls),
+    path("products/", include("products_page.urls") ),
+    path("dimensionalmeasurement/", include("dimensional_measurement.urls")),
+    path("leaktesting/", include("leak_testing.urls")),
+    path("callibrators/", include("callibrators.urls")),
+    path("accessories/", include("accessories.urls")),
+    path("services/", include("services.urls")),
+    path('rosetta/', include('rosetta.urls')), 
+
+)
+
